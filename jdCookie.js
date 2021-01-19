95eb2f14bdd68249c0df6043523397725e5d917b
PengKang <pengkang@xiniaoyun.com>
2021-01-19 10:16:09 +0800

    fix(炸年兽): 修改报错

--- jdCookie.js
+++ jdCookie.js
@@ -1,10 +1,17 @@
 /*
+ * @Author: PengKang
+ * @Date: 2021-01-17 19:19:50
+ * @LastEditors: PengKang
+ * @LastEditTime: 2021-01-19 10:09:12
+ * @FilePath: \scripts\jdCookie.js
+ */
+/*
 此文件为Node.js专用。其他用户请忽略
  */
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJgBj8oADAEzbsZHNZfXAptPnxq833mhWUargfBRDBKFQkbrh0RJK5XVxMjXbt2hobzn2Ov_oQ;pt_pin=18795841631_p',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
